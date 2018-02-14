45610a7ec159a689fbca9211999498d7b83018a6
Estudiantes <Estudiantes@Labing70210>
2018-02-14 18:04:08 -0500

    Taller 1

---
+++ calcu/src/micalculadora/MICALCULADORA.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package micalculadora;
+
+import static java.lang.Math.*;
+
+
+public class MICALCULADORA extends Calculadora {
+    
+    
+    
+   double coseno(){
+   return cos (x);
+   
+   
+}
+}  
+    
+
+  
